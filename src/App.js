292aa29c51c2d84a84afe38e9331c1b85002308e
ensunchoc <ensunchobustamantec@gmail.com>
2026-05-22 17:48:11 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { useAuth } from './context/AuthContext'
 import AuthPage from './pages/AuthPage'
 import Dashboard from './pages/Dashboard'
